2e7de3c0d6a090f3dd92fb1f763059b11892ece4
Adamantcat <juliathehawk@gmail.com>
2017-01-23 17:00:15 +0100

    clulab named entity try

--- src/main/scala/com/ibm/s4cl/project/EmailTopicModel.scala
+++ src/main/scala/com/ibm/s4cl/project/EmailTopicModel.scala
@@ -1,12 +1,30 @@
 package com.ibm.s4cl.project
 
+import org.clulab.processors.corenlp.CoreNLPProcessor
+import org.clulab.processors.shallownlp.ShallowNLPProcessor
+import org.clulab.processors.{Document, Processor}
+import org.clulab.struct.DirectedGraphEdgeIterator
 /**
   *
   */
 object EmailTopicModel {
 
-  def main(args : Array[String]) {
-    println("Hello group member!")
+  def preprocess(text: String): Unit = {
+    val proc: Processor = new CoreNLPProcessor
+    val doc = proc.mkDocument(text)
+    proc.tagPartsOfSpeech(doc)
+    proc.lemmatize(doc)
+    proc.recognizeNamedEntities(doc)
+    doc.clear()
+
+    val sents = doc.sentences
+    sents.foreach(_.entities.foreach(entities => println(s"Named entities: ${entities.mkString(" ")}")))
+  }
+
+  def main(args: Array[String]) {
+    //println("Hello group member!")
+    val test = "John Smith went to China. He visited Beijing, on January 10th, 2013."
+    preprocess(test)
   }
 
 }
