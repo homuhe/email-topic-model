6791ac82f383c33f5b36501e52a409632f955c4e
Neele <Neele.witte@student.uni-tuebingen.de>
2017-01-28 15:40:02 +0100

    Hier die Emailklasse. Die main methode zeigt wie man das abgespeicherte Objekt einlesen und in einer Liste mit allen Emails einspeichern kann.

--- src/main/scala/com/ibm/s4cl/project/EmailTopicModel.scala
+++ src/main/scala/com/ibm/s4cl/project/EmailTopicModel.scala
@@ -6,15 +6,15 @@ package com.ibm.s4cl.project
 
 import scala.io.Source
 import java.io._
-
+import edu.stanford.nlp.simple._
 class Email(file:String) extends scala.Serializable {
   val emailAsText = Source.fromFile(file).getLines().toSeq
   private val numberOfMeta = emailAsText.count(el => el.contains("META"))
   private val adresser = emailAsText(1).replace("FROM-NAME# ", "")
   private val adressee = emailAsText(3).replace("TO-NAME# ", "")
 
-  private val content:String = {
-    emailAsText(emailAsText.indexOf("TEXT-CONTENT# ")+1)
+  private val content: String = {
+    emailAsText(emailAsText.indexOf("TEXT-CONTENT# ") + 1)
   }
 
   private val subject = emailAsText(5).replace("SUBJECT# ", "")
@@ -22,16 +22,48 @@ class Email(file:String) extends scala.Serializable {
   private val adressermail = emailAsText(0)
   private val adresseemail = emailAsText(2)
 
-  def getAdresser : String = this.adresser
+  def getAdresser: String = this.adresser
 
   def getAdressee: String = this.adressee
+
   def getContent = this.content
+
   def getSubject: String = this.subject
+
   def getDate: String = this.date
 
+    def getNamedEntitiysAndNouns: (Map[String, String], Set[String]) = {
+
+      val sent: Sentence = new Sentence(content.toString())
+      val tokens = sent.words().toArray().map(_.toString)
+      val nerTags = sent.nerTags().toArray().map(_.toString)
+      val POSTags = sent.posTags().toArray().map(_.toString)
+      val entitymap = tokens.zip(nerTags).toMap.filter(el => el._2.equals("0"))
+      val wordtags = tokens.zip(POSTags).toMap.filter(el => el._2.equals("NNP")).keySet
+      (entitymap, wordtags)
+    }
+  def getnamedEntities : Map[String, String]= {
+    val sent = new Sentence(content.toString)
+    val tokens = sent.words().toArray().map(_.toString)
+    val nes = sent.nerTags().toArray().map(_.toString)
+    val entities = tokens.zip(nes).toMap.filter(el => el._2.equals("0"))
+    entities
+  }
+  def getContentNouns: Set[String] = {
+    if (content.length > 5) {
+
+      val sent = new Sentence(content)
+      val tokens = sent.words().toArray().map(_.toString)
+      val tags = sent.posTags().toArray().map(_.toString)
+      tokens.zip(tags).toMap.filter(el => el._2.equals("NNP")).keySet
+    }
+    else {
+      Set()
+    }
+  }
 }
 
-object  Email{
+object  Email {
   def main(args: Array[String]): Unit = {
     import java.io.File
     def getListOfFiles(dir: String): List[File] = {
@@ -50,10 +82,8 @@ object  Email{
     objectout.writeObject(emails)
     objectout.close()
     out.close()
-    val in = new FileInputStream("/home/neele/Dokumente/Wikimails")
-    val inObject = new ObjectInputStream(in)
-    val newmails = inObject.readObject().asInstanceOf[List[Email]]
-    println(newmails.size)
+    println("emails have been read")
+
 
   }
 
