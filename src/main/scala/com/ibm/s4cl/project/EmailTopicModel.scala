231d939aab5265efc935294516b5397a124693d7
Adamantcat <juliathehawk@gmail.com>
2017-01-29 13:19:46 +0100

    Aktueller Stand

--- src/main/scala/com/ibm/s4cl/project/EmailTopicModel.scala
+++ src/main/scala/com/ibm/s4cl/project/EmailTopicModel.scala
@@ -1,71 +1,63 @@
 package com.ibm.s4cl.project
 
+import java.io.{FileInputStream, ObjectInputStream}
 import edu.stanford.nlp.simple._
 
 object EmailTopicModel {
 
   def main(args: Array[String]) {
 
-    val text1 = "The fate of Lehman Brothers," +
-      "the beleaguered investment bank, hung in the " +
-      "balance on Sunday as Federal Reserve officials and" +
-      "the leaders of major financial institutions continued to" +
-      "gather in emergency meetings trying to complete a plan to rescue " +
-      "the stricken bank.  Several possible plans emerged from the talks," +
-      "held at the Federal Reserve Bank of New York and led by Timothy R. Geithner," +
-      "the president of the New York Fed, and Treasury Secretary Henry M. Paulson Jr."
+    val in = new FileInputStream("C:\\Users\\Julia\\Dropbox\\SCALA-PROJECT_2017\\wikimails")
+    val inObject = new ObjectInputStream(in)
+    val mails = inObject.readObject().asInstanceOf[List[Email]]
+    println(mails.size)
 
-    val text = "I hope you've been well." +
-      "Following up on the Secretary's request, " +
-      "when would you like to regroup about communications " +
-      "support in Pakistan? As food for thought, I've been talking " +
-      "with She's a great source of insight and information on the " +
-      "ground, with a valuable independent perspective. She is very " +
-      "tuned into the government's cornmunications challenges and " +
-      "opportunities. In her view, while the need is more urgent " +
-      "than ever, the government does have positive programs and " +
-      "policies that would generate good will if people knew about them."
+    mails.take(1000).par.foreach(mail => {
 
-    val sent: Sentence = new Sentence(text1)
-    val tokens = sent.words().toArray()
-    val nerTags = sent.nerTags().toArray()
-    val POSTags = sent.posTags().toArray()
+      val sent: Sentence = new Sentence(mail.getContent)
+      val tokens = sent.words().toArray()
+      val nerTags = sent.nerTags().toArray()
+      val POSTags = sent.posTags().toArray()
 
-    println(tokens.length)
-    println(nerTags.length)
-    println(POSTags.length)
+      println(tokens.length)
+      println(nerTags.length)
+      println(POSTags.length)
 
 
+      /*for (i <- 0 until tokens.length)
+        println(tokens(i), POSTags(i), nerTags(i))*/
 
-    for (i <- 0 until tokens.length)
-      println(tokens(i), POSTags(i), nerTags(i))
+      /* print("\nNamed Entities: ")
+       for (i <- 0 until tokens.length)
+         if (!nerTags(i).equals("O"))
+           println(tokens(i), nerTags(i)) */
 
-    print("\nNamed Entities: ")
-    for (i <- 0 until tokens.length)
-      if (!nerTags(i).equals("O"))
-        println(tokens(i), nerTags(i))
-
-    //fasst multi-token named entities zu einem einzelnen String zusammen
-    val multiNEs: Map[String, String] = {
-      var ne = ""
-      var currentTag = "O"
-      var NEMap: Map[String, String] = Map()
-      for (i <- 0 until nerTags.length) {
+      val multiNEs: Map[String, String] = {
+        var ne = ""
+        var currentTag = "O"
+        var neMap: Map[String, String] = Map()
+        for (i <- 0 until nerTags.length) {
           if (nerTags(i).equals(currentTag) && !currentTag.equals("O"))
             ne += " " + tokens(i)
           else {
-            if(!currentTag.equals("O"))
-              NEMap += (ne -> currentTag)
+            if (!currentTag.equals("O"))
+              neMap += (ne -> currentTag)
             currentTag = nerTags(i).toString
             ne = tokens(i).toString
           }
+        }
+        neMap
       }
-      NEMap
-    }
 
-    println()
-    println("Test multi-token NEs:")
-    multiNEs.foreach(println(_))
+      println()
+      println("Test multi-token NEs:")
+      multiNEs.foreach(println(_))
+
+      val personsMentioned = multiNEs.filter(_._2.equals("PERSON"))
+      val topics = multiNEs.filterNot(_._2.equals("PERSON"))
+      println(personsMentioned)
+      println(topics)
+    })
   }
 
 }
