4da6327e4fd3bbd6ddbdf1de25e7f4a006351f03
Adamantcat <juliathehawk@gmail.com>
2017-01-31 10:40:28 +0100

    Final Version

--- src/main/scala/com/ibm/s4cl/project/Emailreader.scala
+++ src/main/scala/com/ibm/s4cl/project/Emailreader.scala
@@ -34,11 +34,11 @@ class emailreader(emails: List[Email]){
   def getAdresser_TopicMap(person:String) : scala.collection.mutable.Map[String, List[(String, String)]]={
     val adressermap = scala.collection.mutable.Map[String, List[(String, String)]]()
     emails.foreach{email =>
-    val entities = email.namedEntities.toList
-    val p = person match {
-      case "adresser" => email.getAdresser
-      case "adressee" => email.getAdressee
-    }
+      val entities = email.namedEntities.toList
+      val p = person match {
+        case "adresser" => email.getAdresser
+        case "adressee" => email.getAdressee
+      }
       val value = adressermap.getOrElse(p, List())
       val newvalue = entities ::: value
       adressermap.update(p, newvalue)
@@ -50,11 +50,11 @@ class emailreader(emails: List[Email]){
   def getTopicsbyContentWords(person:String) : scala.collection.mutable.Map[String, List[String]] = {
     val adressermap = scala.collection.mutable.Map[String, List[String]]()
     emails.foreach{email =>
-    val nouns = email.contenNouns
-    val p = person match {
-      case "adresser" => email.getAdresser
-      case "adressee" => email.getAdressee
-    }
+      val nouns = email.contenNouns
+      val p = person match {
+        case "adresser" => email.getAdresser
+        case "adressee" => email.getAdressee
+      }
       val value = adressermap.getOrElse(p, List())
       val newvalue = value ::: nouns
       adressermap.update(p, newvalue)
@@ -66,8 +66,8 @@ class emailreader(emails: List[Email]){
   def getTopicsOverTime : scala.collection.mutable.Map[String, Set[(String, String)]] = {
     val timemap = scala.collection.mutable.Map[String, Set[(String, String)]]()
     emails.foreach{email =>
-    val time = email.getDate
-    val topics = email.namedEntities
+      val time = email.getDate
+      val topics = email.namedEntities
       val value = timemap.getOrElse(time, Set())
       timemap.update(time, topics.union(value))
     }
@@ -79,7 +79,7 @@ object emailreader {
   def main(args: Array[String]): Unit = {
     import java.io.{FileOutputStream, ObjectOutputStream}
 
-    val in = new FileInputStream("/home/neele/Dokumente/Wikimails")
+    val in = new FileInputStream("C:\\Users\\Julia\\Dropbox\\SCALA-PROJECT_2017\\Wikimails")
     val inObject = new ObjectInputStream(in)
     val newmails = inObject.readObject().asInstanceOf[List[Email]]
     println(newmails.length)
--- src/main/scala/com/ibm/s4cl/project/TopicModel.scala
+++ src/main/scala/com/ibm/s4cl/project/TopicModel.scala
@@ -1,27 +1,30 @@
 package com.ibm.s4cl.project
+
 import scala.collection.immutable.ListMap
+
 /**
   * Created by root on 29.01.17.
   */
-class TopicModel(mails:List[Email]) {
+class TopicModel(mails: List[Email]) {
   val reader = new emailreader(mails)
   val adresser_namedEntityMap = reader.getAdresser_TopicMap("adresser")
   val adressee_namedEntityMap = reader.getAdresser_TopicMap("adressee")
-  val timemap = reader.getTopicsOverTime
+  val timemap = reader.getNounsOverTime
   val adresser_contentWordMap = reader.getTopicsbyContentWords("adresser")
   val personsMentionedMap = reader.getPersonsMentionedWithTopics
   val topicsOverTime = reader.getTopicsOverTime
   val adressee_contentWordMap = reader.getTopicsbyContentWords("adressee")
 
-  def getFrequentNouns(map:Map[String, List[String]]) = {
+  def getFrequentNouns(map: Map[String, List[String]]) = {
     val normalizedmap = map map { case (key, value) => (key, value.map(_.toLowerCase)) }
     val sortedMap = normalizedmap map { case (key, value) => (key, value.groupBy(el => el).map(e => (e._1 -> e._2.length))) }
-    println("hier")
+   // println("hier")
     val topTen = sortedMap map { case (key, mapvalue) => (key, mapvalue.filter(e => e._2 > 3)) }
-    topTen map {case (key, map) => (key, ListMap(map.toSeq.sortWith(_._2 > _._2):_*))}
+    topTen map { case (key, map) => (key, ListMap(map.toSeq.sortWith(_._2 > _._2): _*)) }
   }
 
   def getAllAdresser = adresser_namedEntityMap.keySet
+
   def getAllAdressee = adressee_namedEntityMap.keySet
 
   def getMostFrequentCommunications: Set[(String, String)] = {
@@ -29,14 +32,15 @@ class TopicModel(mails:List[Email]) {
     communications.groupBy(el => el).map(e => e._1 -> e._2.length).keySet.take(10)
   }
 
-  def getTopicsOverTime(timemap: Map[String, Set[(String, String)]]) : Map[(String, String), Set[(String, String)]] ={
-    val time = timemap map { case (key, value) => ((key.split("-")(0), key.split("-")(1)), value)}
-    val sorted = ListMap(time.toSeq.sortBy(_._1):_*).toMap
+  def getTopicsOverTime(timemap: Map[String, Set[(String, String)]]): Map[(String, String), Set[(String, String)]] = {
+    val time = timemap map { case (key, value) => ((key.split("-")(0), key.split("-")(1)), value) }
+    val sorted = ListMap(time.toSeq.sortBy(_._1): _*).toMap
     sorted
   }
-  def getNounsOverTime(timemap: Map[String, Set[String]]) : Map[(String, String),  Set[String]] = {
-    val time = timemap map {case (key, value) => ((key.split("-")(0), key.split("-")(1)), value)}
-    val sorted = ListMap(time.toSeq.sortBy(_._1):_*)
+
+  def getNounsOverTime(timemap: Map[String, Set[String]]): Map[(String, String), Set[String]] = {
+    val time = timemap map { case (key, value) => ((key.split("-")(0), key.split("-")(1)), value) }
+    val sorted = ListMap(time.toSeq.sortBy(_._1): _*)
     sorted
   }
 }
@@ -46,24 +50,47 @@ object TopicModel {
     import java.io._
 
 
-    val in = new FileInputStream("/home/neele/Dokumente/Wikimails")
+    val in = new FileInputStream("C:\\Users\\Julia\\Dropbox\\scala-project_2017\\Wikimails")
     val inObject = new ObjectInputStream(in)
     val newmails = inObject.readObject().asInstanceOf[List[Email]]
     println("emails have been read")
 
     val model = new TopicModel(newmails)
+   // val model = new TopicModel(Nil)
     println("maps have been created")
-    while(true){
-      if (userinput.equals("adressee")){
+    while (true) {
+      println("options: \n #addressee, #addresser, #frequent-nouns, #timemap, #named-entities, #communications")
+      val input = userinput
+      if (input.equals("#addressee")) {
         println(model.getAllAdressee)
       }
-      else {
-        println("hey")
+      else if (input.equals("#addresser")) {
+        println(model.getAllAdresser)
       }
+      else if (input.equals("#frequent-nouns")) {
+        println("please enter the person you're interested in")
+        val name = userinput
+        println(model.getFrequentNouns(model.adresser_contentWordMap.toMap).getOrElse(name, "person not in Map"))
+      }
+      else if (input.equals("#timemap")) {
+        // timemap = model.adresser_namedEntityMap.toMap.map(el => (el._1, el._2.toSet))
+        model.timemap.foreach { case (key, value) => println(key, value) }
+      }
+      //Hillary Clinton, Lauren Jiloty, Prime Minister, World Bank
+      else if (input.equals("#named-entities")) {
+        println("please enter the person you're interested in")
+        val name = userinput
+        println(model.adresser_namedEntityMap.getOrElse(name, "not in map"))
+      }
+      else if (input.equals("#communications")) {
+        println(model.getMostFrequentCommunications)
+      }
+
     }
     def userinput = {
       import scala.io.StdIn
-      StdIn.readLine().toString}
+      StdIn.readLine().toString
+    }
 
   }
 }
